9fdfe005ba729ff0958d767c593487fa62b4fef1
haxer-max <haxer_max@users.noreply.github.com>
2020-06-09 13:08:31 +0530

    added all basic calc functions and pow

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-document.write("hey");
 var a =document.getElementById("area");
 function press(i)
 {
     a.defaultValue+=i;
+}
+function Clear()
+{
+    a.defaultValue="";
+}
+function Eval()
+{
+    a.defaultValue=eval(a.defaultValue.replace("pow","Math.pow"));
+}
+function back()
+{
+    a.defaultValue=a.defaultValue.substring(0,a.defaultValue.length-1);
 }
