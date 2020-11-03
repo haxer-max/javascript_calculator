99fd5eaedbc932c17e293b313a72d2c6df807ed6
Kaustubh Verma <vermakaustubh28@gmail.com>
2020-11-04 00:11:35 +0530

    solve error of alphabet typo in area field and some minor modifications

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-var a =document.getElementById("area");
-function press(i)
-{
+// mekaustubh28 starts I have disabled the keyboard as using keyboard user can also type alphabets
+//  , which is not required
+document.getElementById("area").readOnly = true;
+// mekaustubh28 ends
+
+
+
+var a = document.getElementById("area");
+
+function press(i) {
     console.log(i)
-    a.value+=i;
+    a.value += i;
 }
-function Clear()
-{
-    a.value="";
+
+function Clear() {
+    a.value = "";
 }
 
 //THIS FUNCTION WILL HELP IN REMOVING THE ISSUE WITH FLOATING POINT NUMBERS
@@ -15,18 +22,16 @@ function strip(number) {
     return (no * 1).toString();
 }
 
-function Eval()
-{
-    let p =a.value;
-    while(p.includes("^"))
-    {
-        p=p.replace('^',"**");
+function Eval() {
+    let p = a.value;
+    while (p.includes("^")) {
+        p = p.replace('^', "**");
     }
     console.log(p);
     let temp = eval(p);
-    a.value=strip(temp);
+    a.value = strip(temp);
 }
-function back()
-{
-    a.value=a.value.substring(0,a.value.length-1);
+
+function back() {
+    a.value = a.value.substring(0, a.value.length - 1);
 }
